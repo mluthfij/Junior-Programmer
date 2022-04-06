fa5533d1b3e3a4a4d041b9279d8c1ca845144687
Muhammad Luthfi Juliansyah <luthfujuliansyah48@gmail.com>
2022-04-06 14:25:54 +0700

    unit 1 and 2

---
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    public string inputID;
+
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        // Move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        
+        if(Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Junior-Programmer
+
+Tugas Game Programming\
+Nama : Muhammad Luthfi Juliansyah\
+NPM : 0619103024\
+Kelas : Reg B1 IF
